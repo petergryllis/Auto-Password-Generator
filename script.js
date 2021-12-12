b8510a7c440a33755efd5abd8a043d53f3e30f1e
Peter Gryllis <petergryllis@riacci.com>
2021-12-12 13:34:59 +1100

    generate random functions

--- script.js
+++ script.js
@@ -80,5 +80,23 @@ function array(low, high) {
 }
 
 
+
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
 // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword)
