b279c39e2bf6ee26f6dea0dee7621065521aaf99
Peter Gryllis <petergryllis@riacci.com>
2021-12-12 14:32:16 +1100

    remove Eventlistener

--- script.js
+++ script.js
@@ -40,6 +40,21 @@ var randomFunc = {
   symbol: getRandomSymbol
 }
 
+// clipboard.addEventListener('click', () => {
+// 	const textarea = document.createElement('textarea');
+// 	const password = resultEl.innerText;
+	
+// 	if(!password) { return; }
+	
+// 	textarea.value = password;
+// 	document.body.appendChild(textarea);
+// 	textarea.select();
+// 	document.execCommand('copy');
+// 	textarea.remove();
+// 	alert('Password copied to clipboard');
+// });
+
+
 //event listener to take values from form in HTML
 generate.addEventListener("click", e => {
   var takecharacterLength = lengthHTML.value;
